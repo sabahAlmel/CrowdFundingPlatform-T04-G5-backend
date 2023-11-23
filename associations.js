219d10aaec26342b4eede97d2acaf0187b11c204
Hasan Deeb <sektories7@gmail.com>
2023-11-23 16:43:16 +0200

    Donation controller

--- associations.js
+++ associations.js
@@ -1,11 +1,9 @@
 import User from "./models/User.models.js";
 import Creator from "./models/Creator.models.js";
-
 import Campaign from "./models/campaignModel.js";
 import Donor from "./models/donor.js";
 import Donations from "./models/donations.js";
 import sequelize from "./config/dbConnection.js";
-import User from "./models/User.models.js";
 
 Creator.hasOne(User);
 User.belongsTo(Creator);
@@ -20,4 +18,4 @@ User.belongsTo(Donor)
 // await Donor.sync({alter: true})
 // await User.sync({alter: true})
 
-await sequelize.sync({alter: true}) 
+// await sequelize.sync({alter: true}) 
