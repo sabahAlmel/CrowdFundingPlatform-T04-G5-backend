e529bfa9b22c861c578366514c655dd075c8af9a
Hasan Deeb <95194832+HasanDeeb7@users.noreply.github.com>
2023-12-13 19:19:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://crowdfunding-9mad.onrender.com",
     credentials: true,
     optionSuccessStatus: 200,
   })
